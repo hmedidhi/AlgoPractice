9e912e9d7f48f0ce8704915290b5ac11300a9841
hmedidhi <mhkrishna.krishna@gmail.com>
2023-07-22 23:55:54 +0530

    Linked list has cycle

--- src/com/practice/ds/linkedlist/LinkedListCycle.java
+++ src/com/practice/ds/linkedlist/LinkedListCycle.java
@@ -1,5 +1,17 @@
 package com.practice.ds.linkedlist;
 
 public class LinkedListCycle {
-
+    public boolean hasCycle(ListNode head) {
+        ListNode node1 = head;
+        if (node1 != null) {
+            ListNode node2 = head.next;
+            while (node2 != null && node2.next != null) {
+                if (node1.val == node2.val)
+                    return true;
+                node1 = node1.next;
+                node2 = node2.next.next;
+            }
+        }
+        return false;
+    }
 }
